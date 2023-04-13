c6e4f02251ce610bd42adcf5def6a725312e8069
Jonathan <jonathanezequiel199728@gmail.com>
2023-04-13 20:59:26 -0300

    conexion local

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -1,12 +1,12 @@
 <?php
 class Conexion {
     static public function conectar(){
-        
-        $link = new PDO("mysql:host=localhost;port=3306;dbname=id19524165_contacto", "id19524165_jeocarp", "O=rl1afl&Z84hM]t");
-        
         /*
-        $link = new PDO("mysql:host=localhost;port=3306;dbname=t.p jonathan ordoñez", "root", "");
+        $link = new PDO("mysql:host=localhost;port=3306;dbname=id19524165_contacto", "id19524165_jeocarp", "O=rl1afl&Z84hM]t");
         */
+        
+        $link = new PDO("mysql:host=localhost;port=3306;dbname=t.p jonathan ordoñez", "root", "");
+        
         $link->exec("set names utf8");
 
         return $link;
